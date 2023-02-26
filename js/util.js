0e0db9c2bf057888da7322f464b468c2ad21f595
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-02-26 20:29:22 +0300

    <refactor>: модульность файло с точкой входа main.js

---
+++ js/util.js
@@ -0,0 +1,17 @@
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+export { getRandomInteger, createIdGenerator };
