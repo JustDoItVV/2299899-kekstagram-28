0bf9aef604e4b7ee0136f4185ed0d87a380dfccd
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-04-01 19:14:21 +0300

    <fix>: показ сообщения с ошибкой при неполучении данных с сервера

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ import { initGallery } from './gallery-mini.js';
 import { setFormSubmit, closeLoadForm } from './form.js';
 import { showSendSuccess } from './form-messages.js';
 import { getData } from './api.js';
+import { showAlert } from './util.js';
 
-getData().then((data) => {
-  initGallery(data);
-});
+getData()
+  .then((data) => initGallery(data))
+  .catch((err) => showAlert(err.message));
 
 setFormSubmit(() => {
   closeLoadForm();
